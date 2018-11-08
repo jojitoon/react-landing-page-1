d338a0a5a57a8b516fd3fb61a542b4bfbbb71ae6
Mauro Guardarini <mauro.guardarini@gmail.com>
2018-11-07 19:28:57 -0500

    test updated node modules

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import { Container, Row, Col } from 'react-grid-system';
 import LandingPage from './components/villages/Landing Page.js';
-import Demo from './components/villages/Demo.js';
+// import Demo from './components/villages/Demo.js';
 
 class App extends React.Component {
   render() {
